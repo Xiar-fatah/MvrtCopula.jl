5ce2815e7bef14604a72f26aa244e528ae0f294b
Documenter.jl <documenter@juliadocs.github.io>
2022-01-20 19:26:46 +0000

    build based on cde2fe5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MvrtCopula","category":"page"},{"location":"#MvrtCopula","page":"Home","title":"MvrtCopula","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MvrtCopula.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MvrtCopula]","category":"page"}]
+}
